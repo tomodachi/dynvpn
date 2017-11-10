b190fcb7a698eb3f1a847af05e65fd73bff41a11
Mateusz Mojsiejuk <mateusz@izettle.com>
2017-11-10 19:07:44 +0100

    removed old style rules

--- scripts/acl_rules/allow_all_to_10.0.0.0_rule.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from template_rule import *
-
-# === ADD your iptables ruleset below  here === ####
-
-os.system("iptables " + ipt_action + " " + ip + " -s " + ip + " -d 10.0.0.0/24 -j ACCEPT")
-
-# === ADD your iptables ruleset above here === ####

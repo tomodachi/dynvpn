26cbafee97b458dce2a05889f88fa86982d063b0
Mateusz Mojsiejuk <mateusz@izettle.com>
2017-11-30 11:17:11 -0800

    hard code python version to use

--- scripts/acl_rules/default_rule.py
+++ scripts/acl_rules/default_rule.py
@@ -7,3 +7,6 @@ ip = argv[1]
 user = argv[2]
 
 syslog.syslog("action: " + " " + rule + " on user: " + user + " ip: " + ip)
+
+# default rules here
+os.system("nft add rule ip filter " + ip + " ip daddr { 10.0.0.1,10.0.3.254 } udp dport 53 accept")
